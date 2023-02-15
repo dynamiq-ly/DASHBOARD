5413ac81d5490e94b26087f3cd1f2cd52c8e14a4
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2023-02-15 19:07:41 +0100

    edit rooms category

--- src/app/main/apps/rooms/store/index.js
+++ src/app/main/apps/rooms/store/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from '@reduxjs/toolkit'
 
 import categories from './categoriesSlice'
 import roomList from './roomListSlice'
+import category from './categorySlice'
 
 const reducer = combineReducers({
   categories,
   roomList,
+  category,
 })
 
 export default reducer
